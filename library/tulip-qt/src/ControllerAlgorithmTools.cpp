7efb9c7e04088a3750db9d26471d40f88a017d35
Patrick Mary <patrick.mary@labri.fr>
2011-10-21 13:49:17 +0000

    fix for sf bug #3426790: ensure correct display of utf8 error messages
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_6_maint@5940 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/ControllerAlgorithmTools.cpp
+++ library/tulip-qt/src/ControllerAlgorithmTools.cpp
@@ -89,7 +89,7 @@ bool ControllerAlgorithmTools::applyAlgorithm(Graph *graph,QWidget *parent,const
   string erreurMsg;
 
   if (!tlp::applyAlgorithm(graph, erreurMsg, dataSet, name, &myProgress  )) {
-    QMessageBox::critical( 0, "Tulip Algorithm Check Failed",QString((name + ":\n" + erreurMsg).c_str()));
+    QMessageBox::critical( 0, "Tulip Algorithm Check Failed", QString::fromUtf8((name + ":\n" + erreurMsg).c_str()));
     // no possible redo
     graph->pop(false);
     ok=false;
@@ -210,7 +210,7 @@ bool ControllerAlgorithmTools::changeProperty(Graph *graph,QWidget *parent,strin
     }
 
     if (!resultBool) {
-      QMessageBox::critical(parent, "Tulip Algorithm Check Failed", QString((name + ":\n" + erreurMsg).c_str()) );
+      QMessageBox::critical(parent, "Tulip Algorithm Check Failed", QString::fromUtf8((name + ":\n" + erreurMsg).c_str()) );
       // no possible redo
       graph->pop(false);
     }
