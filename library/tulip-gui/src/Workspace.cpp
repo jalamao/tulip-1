jalamao/tulip-1

commit b64c2d9bfed38695a582e8c71e88c6dd18c7f2d9

fixed a problem where there was an empty slot when loading a graph that had a view not currently available (e.g. save with matrix view, uninstall matrix view, reopen graph)
git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8183 f5d4c5f9-c943-4916-8a57-bd8605bf961c

library/tulip-gui/src/Workspace.cpp
@@ -187,20 +187,6 @@ void Workspace::panelDestroyed(QObject* obj) {
 
   updateAvailableModes();
 
-  if (!_modeSwitches[currentModeWidget()]->isVisible()) {
-    int maxSize = 0;
-    QWidget* fallbackMode = _ui->startupPage;
-
-    // Current mode is not available, fallback to the largest available mode
-    foreach(QWidget* it, _modeToSlots.keys()) {
-      if (_panels.size() >= _modeToSlots[it].size() && _modeToSlots[it].size() > maxSize) {
-        maxSize = _modeToSlots[it].size();
-        fallbackMode = it;
-      }
-    }
-    switchWorkspaceMode(fallbackMode);
-  }
-
   if (currentModeWidget() == _ui->startupPage) {
     return;
   }
@@ -326,6 +312,20 @@ void Workspace::updatePanels() {
     }
 
     i++;
+  }
+
+  if (!_modeSwitches[currentModeWidget()]->isVisible()) {
+    int maxSize = 0;
+    QWidget* fallbackMode = _ui->startupPage;
+
+    // Current mode is not available, fallback to the largest available mode
+    foreach(QWidget* it, _modeToSlots.keys()) {
+      if (_panels.size() >= _modeToSlots[it].size() && _modeToSlots[it].size() > maxSize) {
+        maxSize = _modeToSlots[it].size();
+        fallbackMode = it;
+      }
+    }
+    switchWorkspaceMode(fallbackMode);
   }
 
   updatePageCountLabel();

commit 2c1e43e46032b7a3f30f9e6c5122eddf260f1be5

Fixed a bug introduced in r8183, tulip was crashing when all the views in the workspace were closed
git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8192 f5d4c5f9-c943-4916-8a57-bd8605bf961c

library/tulip-gui/src/Workspace.cpp
@@ -187,10 +187,6 @@ void Workspace::panelDestroyed(QObject* obj) {
 
   updateAvailableModes();
 
-  if (currentModeWidget() == _ui->startupPage) {
-    return;
-  }
-
   updatePanels();
 }
 
@@ -312,6 +308,11 @@ void Workspace::updatePanels() {
     }
 
     i++;
+  }
+
+  if (currentModeWidget() == _ui->startupPage) {
+    updatePageCountLabel();
+    return;
   }
 
   if (!_modeSwitches[currentModeWidget()]->isVisible()) {